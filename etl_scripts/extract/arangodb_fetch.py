686095ab8a63eb4f61a38dd4d492330e971c089e
Jane <Kim>
2024-09-16 09:20:31 -0700

    creating load and ingest scripts for raw tables

--- etl_scripts/extract/arangodb_fetch.py
+++ etl_scripts/extract/arangodb_fetch.py
@@ -1,15 +1,17 @@
 import gzip
+import json
 import shutil
 import subprocess
 from prefect import task, flow
 
 @task(log_prints=True)
-def export_arangodb_data(output_dir: str = '/tmp/arangodb-dump') -> None:
+def export_arangodb_data(collections: list, output_dir: str = '/tmp/arangodb-dump') -> None:
     """Export arangodb data from arangodump command inside Docker container.
 
     Args:
         output_dir (str, optional): Docker output directory. 
             Defaults to '/tmp/arangodb-dump'.
+        collection (list): List of collections to be exported.
     """
     try:
         # Run arangodump command inside Docker container
@@ -18,12 +20,13 @@ def export_arangodb_data(output_dir: str = '/tmp/arangodb-dump') -> None:
             "arangodump",
             "--server.endpoint", "tcp://127.0.0.1:8529",
             "--server.database", "suttacentral",
-            "--collection", "html_text",
-            "--collection", "sc_bilara_texts",
             "--output-directory", output_dir,
             "--overwrite", "true",
             "--server.password", "test"
         ]
+        for collection in collections:
+            arangodump_cmd.extend(["--collection", collection])
+        
         print("Running command:", " ".join(arangodump_cmd))
         subprocess.run(arangodump_cmd, check=True)
               
@@ -36,32 +39,53 @@ def export_arangodb_data(output_dir: str = '/tmp/arangodb-dump') -> None:
         subprocess.run(docker_cp_cmd, check=True)
         
         print(f'Data exported and copied to {local_output_path}')
+        
     except subprocess.CalledProcessError as e:
         print(f'Error during ArangoDB data dump: {e}')
         
 @task(log_prints=True)
-def extract_gz_file(input_gz_path: str, output_file_path: str) -> None:
+def extract_gz_file(input_gz_path: str, collection: str) -> list:
     """
-    Extract a .gz file and write the decompressed content to an output file.
+    Extract a .gz file in-memory and return the JSON content as a list of dictionaries.
+    Also saves it on disk.
 
     Args:
         input_gz_path (str): Path to the .gz file.
-        output_file_path (str): Path where the decompressed content will be written.
+        collection(str): Name of the collection
+
+    Returns:
+        list: List of parsed JSON objects from the .gz file.
     """
+    json_data = []  # Initialize an empty list to hold multiple JSON objects
+    output_file_path = f'data_dump/{collection}.json'
+    
     try:
-        with gzip.open(input_gz_path, 'rb') as f_in:
-            with open(output_file_path, 'wb') as f_out:
-                shutil.copyfileobj(f_in, f_out)
-        print(f"File extracted to {output_file_path}")
+        # Open and read the gzip file content
+        with gzip.open(input_gz_path, 'rt', encoding='utf-8') as f_in:
+            with open(output_file_path, 'w', encoding='utf-8') as f_out:
+                # Iterate over each line and parse it as a JSON object
+                for line in f_in:
+                    try:
+                        json_obj = json.loads(line.strip())  # Parse each line as JSON
+                        json_data.append(json_obj)  # Add the parsed JSON object to the list
+                        f_out.write(line)  # Write the line to the output file
+                    except json.JSONDecodeError as e:
+                        print(f"Error parsing line: {e}")
+        
+        print(f"Successfully extracted and parsed {len(json_data)} JSON objects from {input_gz_path}")
+        print(f"File written to {output_file_path}")
+        return json_data
+
+
     except Exception as e:
-        print(f"Error extracting {input_gz_path}: {e}")    
+        print(f"Error extracting {input_gz_path}: {e}")
+        return None
         
 if __name__ == "__main__":
     html_in = 'data_dump/arangodb-dump/html_text_8a00c848c7b3360945795d3bc52ebe88.data.json.gz'
     sc_bilara_in = 'data_dump/arangodb-dump/sc_bilara_texts_ede6cd7605f17ff53d131a783fb228e9.data.json.gz'
-    html_out = 'data_dump/html_text.json'
-    sc_bilara_out = 'data_dump/sc_bilara_texts.json'
     export_arangodb_data()
-    extract_gz_file(html_in, html_out)
-    extract_gz_file(sc_bilara_in, sc_bilara_out)
+    extract_gz_file(html_in)
+    extract_gz_file(sc_bilara_in)
+    print('successful')
     
